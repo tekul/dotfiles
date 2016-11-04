a2bf0821d4a50e06c37b3d9dad427daf7aace23f
Luke Taylor <tekul.hs@gmail.com>
2016-11-04 16:20:39 +0000

    Initial commit of simple xmonad setup
    Backup of basic configuration files. They should all just be symlinked
    to their 'dot' equivalents.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,31 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ def
+        { manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts $ layoutHook def
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "darkgreen" "" . shorten 50
+                        }
+        , modMask = mod4Mask
+        , terminal = "urxvt"
+        }
+        `additionalKeys`
+        [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
+        , ((mod4Mask, xK_s), spawn "emacs24-x")
+        ]
+        `additionalKeysP`
+        [ ("<XF86AudioLowerVolume>", spawn "amixer set Headphone 10-")
+        , ("M-<XF86AudioLowerVolume>", spawn "amixer set Headphone 3dB-")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer set Headphone 10+")
+        , ("M-<XF86AudioRaiseVolume>", spawn "amixer set Headphone 3dB+")
+        , ("<XF86AudioMute>", spawn "amixer set Master toggle")
+        , ("M-\\", spawn "synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")
+        ]
