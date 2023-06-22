9ebf1465d50f925be7fad2a8c17ffa7b5566d7cd
Luke Taylor <tekul@pm.me>
2023-06-22 16:42:53 +0100

    Basic fish config files
    Move some aliases to fish abbrs

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    set -gx EDITOR /usr/bin/nvim
+    set -gx fish_greeting "是故滑疑之耀，聖人之所圖也"\n"shìgù huáyí zhī yào, shèngrén zhī suǒ tú yě"
+    fish_vi_key_bindings
+    bind --mode insert --sets-mode default jk repaint
+    source ~/dotfiles/aliases
+    thefuck --alias | source
+end
+
