f74cf73a793c7993a0d474ed2d98508fa22e3ff9
Luke Taylor <tekul@pm.me>
2024-05-08 21:30:39 +0100

    Fix directory colours for folders on FAT partition
    The default colour for directories which are writable by other users was
    hard to read so this overwrites it.
    
    Use 'dircolors --print-ls-colors' to see defaults.

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     set -gx EDITOR /usr/bin/nvim
     set -gx LAUNCH_EDITOR launch_editor_script
     set -gx fish_greeting "是故滑疑之耀，聖人之所圖也"\n"shìgù huáyí zhī yào, shèngrén zhī suǒ tú yě"
+    set -gx LS_COLORS "ow=31"
     fish_vi_key_bindings
     bind --mode insert --sets-mode default jk repaint
     source ~/dotfiles/aliases
