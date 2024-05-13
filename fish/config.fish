d0d2d25503f65010fed2ba0565a15333a06ad3cf
Luke Taylor <tekul@pm.me>
2024-05-13 23:02:07 +0100

    Set BROWSER env variable
    Stops tools like vite from opening chromium when firefox is already
    open.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     set -gx EDITOR /usr/bin/nvim
+    set -gx BROWSER "firefox"
     set -gx LAUNCH_EDITOR launch_editor_script
     set -gx fish_greeting "是故滑疑之耀，聖人之所圖也"\n"shìgù huáyí zhī yào, shèngrén zhī suǒ tú yě"
     set -gx LS_COLORS "ow=31"
