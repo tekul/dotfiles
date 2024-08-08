b27cf421207d0dca87a6dfa83b3c269f6188be98
Luke Taylor <tekul@pm.me>
2024-08-08 19:00:47 +0100

    Set 200ms timeout for key sequences in fish
    Prevents conflicts between use of "jk" to exit insert mode, and
    "jj" for jujutsu commands. Without this fish waits until another
    key is pressed before showing the second j, which is confusing.

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ if status is-interactive
     set -gx LAUNCH_EDITOR launch_editor_script
     set -gx fish_greeting "是故滑疑之耀，聖人之所圖也"\n"shìgù huáyí zhī yào, shèngrén zhī suǒ tú yě"
     set -gx LS_COLORS "ow=31"
+    # 200ms delay for sequences like "jk" below. Otherwise when we type "jj" (jujutsu) it waits 
+    # indefinitely to see if the second j is part of the "jk" sequence
+    set -gx fish_sequence_key_delay_ms 200
     fish_vi_key_bindings
     bind --mode insert --sets-mode default jk repaint
     source ~/dotfiles/aliases
